ca47c7f7cfff6e4c6dab715d5e730b3d14c4865d
Jakub Baran <boxiercarpet@gmail.com>
2024-02-15 09:29:51 +0100

    Zadanie1: Done

--- src/main.py
+++ src/main.py
@@ -1,2 +1,8 @@
 def file_sum(file_path):
-    pass
+    sum = 0
+    with open(file_path, 'r') as file:
+        for line in file:
+            numbers = [int(n) for n in line.split("; ")]
+            for n in numbers:
+                sum += n
+    return sum
