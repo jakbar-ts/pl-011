10cf38e2af6c119e19f12ae6c104f28e47eced0c
Jakub Baran <boxiercarpet@gmail.com>
2024-03-07 09:08:56 +0100

    zadanie6: Done

--- src/zadanie6.py
+++ src/zadanie6.py
@@ -3,7 +3,41 @@ class CaesarCipher:
         self.shift = shift
 
     def encrypt(self, plaintext):
-        pass
+        encrypted_text = ""
+        for char in plaintext:
+            if char.isalpha():
+                shifted = ord(char) + self.shift
+                if char.islower():
+                    if shifted > ord('z'):
+                        shifted -= 26
+                    elif shifted < ord('a'):
+                        shifted += 26
+                elif char.isupper():
+                    if shifted > ord('Z'):
+                        shifted -= 26
+                    elif shifted < ord('A'):
+                        shifted += 26
+                encrypted_text += chr(shifted)
+            else:
+                encrypted_text += char
+        return encrypted_text
 
     def decrypt(self, ciphertext):
-        pass
+        decrypted_text = ""
+        for char in ciphertext:
+            if char.isalpha():
+                shifted = ord(char) - self.shift
+                if char.islower():
+                    if shifted > ord('z'):
+                        shifted -= 26
+                    elif shifted < ord('a'):
+                        shifted += 26
+                elif char.isupper():
+                    if shifted > ord('Z'):
+                        shifted -= 26
+                    elif shifted < ord('A'):
+                        shifted += 26
+                decrypted_text += chr(shifted)
+            else:
+                decrypted_text += char
+        return decrypted_text
